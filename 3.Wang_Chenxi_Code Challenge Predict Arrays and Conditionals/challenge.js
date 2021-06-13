ea5bbda233ecd244f92e055028afd633605d721e
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-06-13 08:18:46 -0700

    6/13/2021

---
+++ 3.Wang_Chenxi_Code Challenge Predict Arrays and Conditionals/challenge.js
@@ -0,0 +1,34 @@
+// http://learn.codingdojo.com/m/165/7107/51588
+// Assignment: Create T-diagrams to predict the output for the following code snippets. 
+
+// Directions: 
+
+// For each code snippet, create a T-diagram. 
+// Create a single text document or photograph of your T-Diagrams that contains the 3 T-diagrams for the code snippets below and submit it.
+// Reminder: This is an opportunity for you to build the habit of showing up for yourself. We won't be reviewing your work, AND your brain needs this workout. Remember, practice a skill regardless of whether a coach or teacher or boss or peer will look at it. This isn't school. This is a dojo.
+
+Code Snippet 1
+function displayContactInfo() {
+    var auntContactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+// ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+
+Code Snippet 2
+function displayGrocerylist() {
+    var produce = ["apples", "oranges"];
+    var frozen = ["broccoli", "ice cream"];
+    frozen.push("hashbrowns");
+    console.log(frozen);
+}
+
+// ["broccoli", "ice cream", "hashbrowns"]
+
+Code Snippet 3
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+// "Bambi", "Beetlejuice", "Toy Story", "Zoro"
