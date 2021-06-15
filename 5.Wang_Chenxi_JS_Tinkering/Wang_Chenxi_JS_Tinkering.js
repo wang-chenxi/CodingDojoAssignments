f070c0b3ab3de99d4eecb876b0604164a7a8cc0d
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-06-14 21:03:37 -0700

    6/14/2021

---
+++ 5.Wang_Chenxi_JS_Tinkering/Wang_Chenxi_JS_Tinkering.js
@@ -0,0 +1,10 @@
+// Optional Code Challenge: JavaScript http://learn.codingdojo.com/m/165/7107/51566 author:Chenxi Wang
+var childHeight = 30;
+function displayIfChlidIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year");
+    }
+}
+displayIfChlidIsAbleToRideTheRollerCoaster();
