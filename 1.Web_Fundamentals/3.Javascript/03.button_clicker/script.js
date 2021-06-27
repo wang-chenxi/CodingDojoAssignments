e0d12579fa9e7d333ea20bd5eb1b9f9c3754126e
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-06-26 21:37:10 -0700

    6/26/2021

--- 1.Web_Fundamentals/3.Javascript/03.button_clicker/script.js
+++ 1.Web_Fundamentals/3.Javascript/03.button_clicker/script.js
@@ -1,3 +1,15 @@
 function ninjaButton() {
     alert("Ninja was liked!")
 }
+
+function logButton(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(element) {
+    element.remove();
+}
