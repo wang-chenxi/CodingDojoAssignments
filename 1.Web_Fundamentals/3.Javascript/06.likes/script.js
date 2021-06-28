8fe0cdb617a54d1efa441d05c6fbbf25bdab46bb
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-06-27 21:43:25 -0700

    2021/6/27

---
+++ 1.Web_Fundamentals/3.Javascript/06.likes/script.js
@@ -0,0 +1,7 @@
+var count = [1, 1, 1];
+var likeCount = [document.querySelector("#like-1"), document.querySelector("#like-2"), document.querySelector("#like-3")];
+function addLike(index) {
+    console.log(index);
+    count[index - 1]++;
+    likeCount[index - 1].innerText = count[index - 1] + " like(s)";
+}
