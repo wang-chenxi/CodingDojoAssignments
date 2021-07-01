c31b7a9db11379aff2f0fc872434a238860f6bff
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-07-01 13:05:12 -0700

    7/1/2021

--- 1.Web_Fundamentals/3.Javascript/08.DojoWeather/script.js
+++ 1.Web_Fundamentals/3.Javascript/08.DojoWeather/script.js
@@ -1,3 +1,5 @@
+var temp = document.querySelectorAll(".covertableTemp");
+
 function display() {
     alert("Loading weather report...")
 }
@@ -7,5 +9,26 @@ function hide(element) {
 }
 
 function convertion(element) {
+    if (element.value == "°C") {
+        toCelsius();
+        console.log(temp);
+    } else {
+        toFahrenheit();
+        console.log(temp);
+    }
     alert("You select " + element.value);
+}
+
+function toCelsius() {
+    for (var i = 0; i < temp.length; i++) {
+        var currentTemp = temp[i].innerText;
+        temp[i].innerText = Math.floor((currentTemp - 32) * 5 / 9);
+    }
+}
+
+function toFahrenheit() {
+    for (var i = 0; i < temp.length; i++) {
+        var currentTemp = temp[i].innerText;
+        temp[i].innerText = Math.floor(currentTemp * 9 / 5 + 32);
+    }
 }
