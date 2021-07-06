931f4fe95eaa0845f1ad3c61eba3bc6c9f068a3b
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-07-06 10:31:12 -0700

    7/6/2021

--- 2.Python/04.fundamentals/5.hello_world/hello_world.py
+++ 2.Python/04.fundamentals/5.hello_world/hello_world.py
@@ -7,7 +7,7 @@ print("Hello " + name)  # with a +
 # 3. print "Hello 42!" with the number in a variable
 name = 42
 print("Hello", 42, "!")  # with a comma
-print("Hello "+42+"!")  # with a +	-- this one should give us an error!
+print("Hello "+str(42)+"!")  # with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
