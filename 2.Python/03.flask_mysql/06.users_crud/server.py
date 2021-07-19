afd063902c7b06d812721f7a5235ecf423594e95
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-07-19 07:59:52 -0700

    7/19/2021

---
+++ 2.Python/03.flask_mysql/06.users_crud/server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, session
+from user import User
+
+# from dataclass import Dataclass
+app = Flask(__name__)
+app.secret_key = 'this is a secret key'
+# python -m pipenv install flask
+# python -m pipenv shell
+# python server.py
+# REMEMBER to never render on a post!
+
+
+@app.route('/users')
+def home():
+    # call the get all classmethod to get all friends
+    users = User.get_all()
+    return render_template('index.html', users=users)
+
+
+@app.route('/users/<id>')
+def show(id):
+    # call the get all classmethod to get all friends
+    data = {'id': id}
+    user = User.get_by_id(data)
+    return render_template('show.html', user=user)
+
+
+@app.errorhandler(404)
+def no_response(not_assigned):
+    return '404- there is no page here. Go to HOME page'
+
+
+@app.route('/form')
+def create():
+    return render_template('create.html')
+
+
+@app.route('/form', methods=['POST'])
+def form():
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    User.save(data)
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 2.Python/03.flask_mysql/06.users_crud/user.py
@@ -0,0 +1,43 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+# Now we use class methods to query our database
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('users').query_db(query)
+        # Create an empty list to append our instances of users
+        users = []
+        # Iterate over the db results and create instances of users with cls.
+        for user in results:
+            users.append(cls(user))
+        return users
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users by id = VALUES%(id)s;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('users').query_db(query, data)
+        user = cls(results[0])
+        return user
+
+    @classmethod
+    def save(cls, data):
+        query = 'INSERT INTO users ( first_name , last_name , email, created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );'
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('users').query_db(query, data)
+
+    # @classmethod
+    # def update(cls, data):
