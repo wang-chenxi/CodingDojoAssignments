09836e48f1dacbba718d7374cc126efae07ccfb4
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-08-02 09:49:43 -0700

    8/2/2021

---
+++ 3.Java/01.Fundementals/02.Your_Own_Java_Program/Introduction.java
@@ -0,0 +1,7 @@
+ public class Introduction {
+   public static void main(String[] args) {
+       System.out.println("My name is Chenxi");
+       System.out.println("I am 31 years old");
+       System.out.println("My hometown is Beijing, China");
+   }
+}
