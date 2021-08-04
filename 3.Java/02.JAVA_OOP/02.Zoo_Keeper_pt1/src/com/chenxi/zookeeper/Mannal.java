abcc452bd12d024d8526ddbad647bc96f875924f
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-08-03 21:10:15 -0700

    8/3/2021

---
+++ 3.Java/02.JAVA_OOP/02.Zoo_Keeper_pt1/src/com/chenxi/zookeeper/Mannal.java
@@ -0,0 +1,15 @@
+package com.chenxi.zookeeper;
+
+public class Mannal {
+
+	protected int energyLevel;
+	
+	public Mannal() {
+		this.energyLevel = 100;
+	}
+	
+public int displayEnergy() {
+	return this.energyLevel;
+}
+	
+}
---
+++ 3.Java/02.JAVA_OOP/02.Zoo_Keeper_pt1/src/com/chenxi/zookeeper/Test.java
@@ -0,0 +1,17 @@
+package com.chenxi.zookeeper;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Gorilla test = new Gorilla();
+System.out.println(test.displayEnergy());
+test.throwSomething();
+System.out.println(test.displayEnergy());
+test.eatBananas();
+System.out.println(test.displayEnergy());
+test.climb();
+System.out.println(test.displayEnergy());
+	}
+
+}
