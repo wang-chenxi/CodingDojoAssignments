890078937b53dfae4bbf7bc6c9c6e49ec02c546f
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-08-03 22:13:43 -0700

    8/3/2021

---
+++ 3.Java/02.JAVA_OOP/04.Object_Master_pt1/src/com/chenxi/objectmaster/Test.java
@@ -0,0 +1,14 @@
+package com.chenxi.objectmaster;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Human Chenxi = new Human();
+Human Richard = new Human();
+
+Chenxi.attack(Richard);
+System.out.println(Richard.getHealth());
+	}
+
+}
