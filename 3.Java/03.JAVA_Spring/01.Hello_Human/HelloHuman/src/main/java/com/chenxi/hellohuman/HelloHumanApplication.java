e8a6dad404760c7640203475e55bdd2f44506a59
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-08-06 13:49:58 -0700

    8/6/2021

---
+++ 3.Java/03.JAVA_Spring/01.Hello_Human/HelloHuman/src/main/java/com/chenxi/hellohuman/HelloHumanApplication.java
@@ -0,0 +1,26 @@
+package com.chenxi.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String human(@RequestParam(value="name",required=false) String name) {
+		if(name != null) {
+			return "<h1>Hello " + name + "!</h1><br>Welcome to SpringBoot!";
+		}else {
+			return "<h1>Hello Human!</h1><br>Welcome to SpringBoot!";		
+		}
+	}
+	
+}
