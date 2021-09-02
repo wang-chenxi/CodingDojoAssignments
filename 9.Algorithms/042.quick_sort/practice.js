866661d073d664c3b19cc0c58dc6d3560861db45
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-09-02 14:46:43 -0700

    9/2/2021

---
+++ 9.Algorithms/042.quick_sort/practice.js
@@ -0,0 +1,123 @@
+/* 
+  Params: nums, left, right
+    - left and right are indexes, for now, left will be 0, and right will be
+        the last idx.
+    - later these params will be used to specify a sub section of the array to
+        partition.
+  Steps:
+  1. Pick an number out of the arr to be your pivot value
+    - usually the middle idx but can be any.
+  2. Partition the array IN PLACE such that all values less than the pivot
+      value are to the left of it and all values greater than the pivot value
+      are to the right (not perfectly sorted).
+  3. return: the index where the left section of smaller items ends.
+  "Choosing a random pivot minimizes the chance that you will encounter
+  worst-case O(n^2) performance (always choosing first or last would cause
+  worst-case performance for nearly-sorted or nearly-reverse-sorted data).
+  Choosing the middle element would also be acceptable in the majority of
+  cases."
+  https://stackoverflow.com/questions/164163/quicksort-choosing-the-pivot
+*/
+
+const nums1 = [11, 8, 14, 3, 6, 2, 7];
+const nums2 = [11, 8, 14, 3, 3, 3, 6, 2, 7];
+const nums3 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
+const nums4 = [5, 2, 3, 1]
+const nums5 = [5, 1, 1, 2, 0, 0]
+const nums6 = [1, 2]
+const nums7 = [2, 1]
+
+/**
+ * Partitions the given array in-place by selecting the number at the middle
+ * index to use it as a "pivot" value, then arranges all numbers less than the
+ * pivot to be to it's left and all larger numbers to the right of the pivot.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @param {number} left The index indicating the start of the slice of array
+ *    being processed.
+ * @param {number} right The index indicating the end of the slice of array
+ *    being processed.
+ * @returns {Array<number>} The idx where left section of smaller items ends.
+ */
+// function partition(nums = [], left = 0, right = nums.length - 1) {
+//     var pivot = nums[Math.floor(nums.length / 2)];
+//     console.log(pivot)
+//     var i = left;
+//     var j = right;
+//     while (i < j) {
+//         if ((nums[i] < pivot) && (nums[j] > pivot)) {
+//             i++;
+//             j--;
+//         }
+//         else if ((nums[i] > pivot) && (nums[j] > pivot)) {
+//             j--;
+//         }
+//         else if ((nums[i] > pivot) && (nums[j] < pivot)) {
+//             var temp = nums[i];
+//             nums[i] = nums[j];
+//             nums[j] = temp;
+//             i++;
+//             j--;
+//         } else {
+//             i++;
+//         }
+//     }
+//     var temp = pivot;
+//     nums[Math.floor(nums.length / 2)] = nums[i]
+//     nums[i] = temp
+//     return nums;
+
+// }
+
+function partition(nums = [], left = 0, right = nums.length - 1) {
+    var pivot = nums[right];
+    var i = left;
+    var j = right - 1;
+    //pivot shouldn't be involved
+    console.log(pivot)
+    while (i < j) {
+        //always check i firstly and stop if we find invalid elements
+        while ((i < j) && (nums[i] < pivot)) {
+            i++;
+        }
+        //check j and stop if we find invalid elements
+        while ((i < j) && (nums[j >= pivot])) {
+            j--;
+        }
+        //swap the element if the condition is still true
+        if (i < j) {
+            var temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    // bring the pivot back to the sorted array
+    if (pivot > nums[j]) {
+        var temp = nums[j + 1];
+        nums[j + 1] = pivot;
+        nums[nums.length - 1] = temp;
+    } else {
+        var temp = nums[j - 1];
+        nums[j - 1] = pivot;
+        nums[nums.length - 1] = temp;
+    }
+
+    //return the result
+    return nums;
+
+}
+
+console.log("nums1:", partition(nums1))
+console.log("nums2:", partition(nums2))
+console.log("nums3:", partition(nums3))
+console.log("nums4:", partition(nums4))
+console.log("nums5:", partition(nums5))
+console.log("nums6:", partition(nums6))
+console.log("nums7:", partition(nums7))
+
+module.exports = { partition };
+
+
